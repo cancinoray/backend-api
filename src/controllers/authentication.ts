12351368466679bba2f7633a371a0dd8b97e902a
Ray Cancino <cancinoray@gmail.com>
2023-10-31 01:22:01 +0800

    refactor the code

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -6,22 +6,13 @@ import { random, authentication } from '../helpers';
 export const login = async (req: express.Request, res: express.Response) => {
   try {
     const {email, password} = req.body;
-
-    if(!email || !password ) {
-      return res.sendStatus(400)
-    }
+    if(!email || !password ) return res.sendStatus(400)
 
     const user = await getUsersByEmail(email).select('+authentication.salt + authentication.password')
-
-    if(!user) {
-      return res.sendStatus(400)
-    }
+    if(!user) return res.sendStatus(400)
 
     const expectedHash = authentication(user.authentication.salt, password);
-
-    if(user.authentication.password !== expectedHash) {
-      return res.sendStatus(403)
-    }
+    if(user.authentication.password !== expectedHash) return res.sendStatus(403)
 
     const salt = random();
     user.authentication.sessionToken = authentication(salt, user._id.toString())
@@ -41,16 +32,10 @@ export const login = async (req: express.Request, res: express.Response) => {
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const {email, password, username} = req.body;
-
-    if(!email || !password || !username) {
-      return res.sendStatus(400)
-    }
+    if(!email || !password || !username) return res.sendStatus(400)
 
     const existingUser = await getUsersByEmail(email);
-
-    if(existingUser) {
-      return res.sendStatus(400)
-    }
+    if(existingUser) return res.sendStatus(400)
 
     const salt = random();
     const user = await createUser({
