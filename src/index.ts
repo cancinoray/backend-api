fa07bec26008fbf29fd8495f68757942a112b5e7
Ray Cancino <cancinoray@gmail.com>
2023-10-31 01:18:47 +0800

    authentication routing and logging in

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from './router';
+
 
 const app = express();
 
@@ -27,4 +29,6 @@ const MONGO_URL = 'mongodb+srv://cancinoray:6ZXmYuOa2JAYh9NI@cluster0.xtah2yv.mo
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error))
+
+app.use('/', router())
 
